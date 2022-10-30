74f0eea8407fe7cd4d83d02198031543715ac811
Bhavesh Sharma <sbhavesh@vmware.com>
2022-10-30 16:02:01 +0530

    commit after adding in some aesthetic changes and echo statements

--- README.md
+++ README.md
@@ -3,11 +3,16 @@ This project installs apache2 web server, adds it as a service and creates a cro
 
 Following variables can be updated to configure the script
 
-s3_bucket_name=upgrad-bhavesh - The s3 bucket name to be used
-tar_file_path=/tmp - path on the local system when the tar files should reside
-tar_file_name=$name-httpd-logs-$timestamp.tar - the name of the tar file
+s3_bucket_name - The s3 bucket name to be used
+
+tar_file_path - path on the local system when the tar files should reside
+
+tar_file_name - the name of the tar file that will be uploaded to s3, defaults to bhavesh-httpd-logs-<timestamp>.tar
+
 log_path=/var/log/apache2 - the path where the apache2 logs reside
-inventory_file_name=/var/www/html/inventory.html - the path and name of the inventory file 
-cron_file_location=/etc/cron.d/automation - location of the cron jobfile
+
+inventory_file_name=/var/www/html/inventory.html - the path and name of the inventory file that is used to display all the tar files present
+
+cron_file_location=/etc/cron.d/automation - location of the cron job file
 
   
--- automation.sh
+++ automation.sh
@@ -1,5 +1,18 @@
-#!/bin/sh
+########################################################################################################################
+# !/bin/bash
+# Author: Bhavesh
+# Description: The script does the following
+#             - updates packages on the EC2 machine
+#             - installs the apache web server if not present
+#             - creates a service for the web server if not present, starts it if it is not running
+#             - archives the log files present at /var/log/apache2 into tar files, uploads the tar files to the aws s3
+#               bucket specified by the variable "s3_bucket"
+#             - updates the size of the tar file and time stamp into the inventory file - /var/www/html/inventory.html
+#                which acts as the index of the uploaded s3 files
+#             - creates a cron job to periodically archive and upload the logs
+########################################################################################################################
 
+# variable declaration
 s3_bucket_name=upgrad-bhavesh
 timestamp=$(date '+%d%m%Y-%H%M%S')
 tar_file_name=bhavesh-httpd-logs-$timestamp.tar
@@ -7,17 +20,13 @@ log_path=/var/log/apache2
 inventory_file=/var/www/html/inventory.html
 cron_file_location=/etc/cron.d/automation
 
-echo $tar_file_name
-
 #udpate packages
 echo "updating packages..."
 sudo apt update -y
-
 echo "---------------------------------------------------------"
 
 #check if apache2 is installed
-echo "checking if apache2 is installed"
-#sudo apt show apache2
+echo "checking if apache2 is installed..."
 sudo dpkg -s apache2
 apache2_installed=$?
 echo "---------------------------------------------------------"
@@ -25,7 +34,7 @@ echo "---------------------------------------------------------"
 #if apache2 is not installed
 if [ $apache2_installed -ne 0 ]
 then
-	echo "apache2 is not installed, will install now"
+	echo "apache2 is not installed, installing apache2..."
 	#install apache 2
 	sudo apt install apache2 -y
 	err=$?
@@ -35,73 +44,59 @@ then
 		exit 1
 	fi
 fi
-
 echo "---------------------------------------------------------"
 
 #Is apache2 enabled for systemctl, if not enable it
-echo "checking if apache2 is enabled"
+echo "checking if apache2 service is enabled..."
 sudo systemctl is-enabled apache2
 if [ $? -ne 0 ]
 then
-	echo "enabling the apache2"
+	echo "apache2 service is not enabled, enabling the apache2 service..."
 	sudo systemctl enable apache2
 fi
-
 echo "---------------------------------------------------------"
 
 #check if apache2 is active, else start it
-echo "checking if apache2 is active"
+echo "checking if apache2 service is active..."
 sudo systemctl is-active apache2
 if [ $? -ne 0 ]
 then
-	echo "starting the apache2 service"
+	echo "apache2 service is not active, starting the apache2 service..."
 	sudo systemctl start apache2
 fi
-
 echo "---------------------------------------------------------"
 
 #create the tar file for *.log files under /tmp
-echo "creating the tar file now"
+echo "creating the tar file now..."
 tar -cvf /tmp/$tar_file_name $log_path/*.log
-
 echo "---------------------------------------------------------"
 
-
 #check if awscli is present
+echo "checking if aws cli is installed..."
 sudo dpkg -s awscli
 aws_cli_installed=$?
-
-
 if [ $aws_cli_installed -ne 0 ]
 then
-	echo "installing the aws cli..."
+	echo "aws cli is not installed, installing the aws cli..."
 	sudo apt install awscli -y
 	if [ $? -ne 0 ]
 	then
 		echo "Error installing the aws cli..."
+		exit 1
 	fi
 fi
+echo "---------------------------------------------------------"
 
 #upload the tar file into s3
-echo "This is to upload the tar"
+echo "uploading the archived tar file to s3 bucket..."
 aws s3 cp /tmp/$tar_file_name s3://$s3_bucket_name/$tar_file_name
+aws_upload_successful=$?
 
-
-tar_file_size=$(ls -lh /tmp/$tar_file_name | awk '{print $5}')
-
-# Inventory file relatd
-if [ -e $inventory_file ]
+if [ $aws_upload_successful -ne 0 ]
 then
-	echo "inventory file is already present, hence will be updated..."
-	echo "<p>httpd-logs &emsp;&emsp;&emsp;&emsp; $timestamp &emsp;&emsp;&emsp;&emsp; tar &emsp;&emsp;&emsp;&emsp; $tar_file_size </p>" >> $inventory_file
+  echo "error uploading to s3..."
+  ext 1
 else
-	echo "inventory file is not present. will create one now..."
-	echo "<h3>Log &emsp;&emsp;&emsp;&emsp; Time created &emsp;&emsp;&emsp;&emsp; Type &emsp;&emsp;&emsp;&emsp; Size</h3>" > $inventory_file
-	echo "<p>httpd-logs &emsp;&emsp;&emsp;&emsp; $timestamp &emsp;&emsp;&emsp;&emsp; tar &emsp;&emsp;&emsp;&emsp; $tar_file_size </p>" >> $inventory_file
+  echo "successfully uploaded to the s3 bucket..."
 fi
-
-if [ ! -e $cron_file_location ]
-then
-	echo "cron job does not exist, adding one now..."
-	echo "0 0 * * * root /root/Automation_Project/automation.sh" > $cron_file_location
-fi
+echo "---------------------------------------------------------"
